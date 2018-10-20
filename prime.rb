cc60c47a4a367f45c332ca8705fc3cf26eeffb9b
Robert Chen <robertchen234@gmail.com>
2018-10-20 20:11:21 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,19 +1,4 @@
 # Add  code here!
 def prime?(num)
-  i = 2 
-  prime = 0
-  while i <= 9
-    j = 2
-    while j <= 9 
-      if i * j = num 
-        prime = false 
-      end 
-      j += 1 
-    end
-    i += 1
-  end 
-  if prime == 0 
-    true 
-  end 
-  prime 
+  
 end 
