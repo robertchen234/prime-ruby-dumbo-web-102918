cf032cbb4a512ca76155646293d45c852dc55527
Robert Chen <robertchen234@gmail.com>
2018-10-20 20:31:09 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
 # Add  code here!
+def prime?(num)
+  i = 2
+  prime = true
+  while i <= 9 
+    if num % i == 0 || num < 2 || num % i == 1 unless num == 2
+      prime = false 
+    end
+  i += 1
+  end 
+  prime
+end 
